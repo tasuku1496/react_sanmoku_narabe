ebd2cb4d7b3ff2ae31d05ccf4228c988d95fd78c
tasuku1496 <tishiguro@careerindex.co.jp>
2023-12-15 09:45:10 +0900

    勝者のマスをハイライトする処理を追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick, highlight }) {
+  console.log(highlight)
+  const className = `square ${highlight ? 'highlight' : ''}`;
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className={className} onClick={onSquareClick}>
       {value}
     </button>
   );
@@ -27,30 +29,36 @@ function Board({ xIsNext, squares, onPlay }) {
   }
 
   const winner = calculateWinner(squares);
+  const winnerInfo = calculateWinner(squares);
+
   let status;
   if (winner) {
-    status = 'Winner: ' + winner;
+    status = 'Winner: ' + winner["winner"];
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
+
+// 勝利マスをハイライトするための配列を生成
+const highlight = squares.map((_, index) =>
+  winnerInfo && winnerInfo.winningSquares.includes(index));
 
   return (
     <div>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} highlight={highlight[0]} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} highlight={highlight[1]} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} highlight={highlight[2]} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} highlight={highlight[3]} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} highlight={highlight[4]} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} highlight={highlight[5]}/>
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} highlight={highlight[6]}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} highlight={highlight[7]}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} highlight={highlight[8]}/>
       </div>
     </div>
   );
@@ -115,7 +123,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return { winner: squares[a], winningSquares: [a, b, c] };
     }
   }
   return null;
